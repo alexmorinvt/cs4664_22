7a59f417ca092313b0c74036c8f05c824a0d328f
Alexey Didenkov <adidenkov@vt.edu>
2022-04-29 10:23:31 -0400

    Sweep: load previous files

--- utils/hyper.py
+++ utils/hyper.py
@@ -1,10 +1,13 @@
+from gettext import find
 from typing import Type
-from model import Model
 from itertools import product
-from utils.crossval import cross_validate
-import os
+from click import confirm
 from traceback import print_exc
-from json import dump
+from json import dump, load, JSONDecodeError
+import os
+
+from utils.crossval import cross_validate
+from model import Model
 from example_models import Hold
 
 
@@ -21,36 +24,10 @@ def sweep(model: Type[Model], **cross_args):
     Produces:
         Validation results in a .json file.
     """
-    stock, text = cross_args['train_val']
-    assert(len(stock) == len(text))
-    for s, t in zip(stock, text):
-        assert(s.columns.name == t.columns.name)
-    if 'baseline_factory' not in cross_args:
-        cross_args['baseline_factory'] = lambda fees: Hold(fees)
-    results = {
-        'model': model.__name__,
-        'simulation': {
-            'fees': cross_args['sim'].fees,
-            'principal': cross_args['sim'].principal,
-        },
-        'validation': {
-            'method': cross_args['partition'].__name__,
-            'split': cross_args['split'],
-            'folds': cross_args['folds'],
-        },
-        'data': {
-            'stock': [s.columns.name for s in stock],
-            'len_stock': [len(s) for s in stock],
-            'len_text': [len(t) for t in text],
-            'begin': str(min(s.iloc[0].date_time for s in stock)),
-            'end': str(max(s.iloc[-1].date_time for s in stock)),
-        },
-        'baseline': cross_args['baseline_factory'](cross_args['sim'].fees).__class__.__name__
-            if cross_args['baseline_factory'] is not None else 'None',
-        'sweep': list(),
-    }
+    results = gen_config(model, cross_args)
     filename = SWEEP_DIR + results['model'] + ".json"
     os.makedirs(os.path.dirname(filename), exist_ok=True)
+    previous = check_previous(filename, results)
 
     # Try every combination of parameters
     names, configs = zip(*model.config.items())
@@ -58,10 +35,23 @@ def sweep(model: Type[Model], **cross_args):
         params = {name: value for name, value in zip(names, values)}
         print(f"\nParameters: {params}")
         
+        # Check if result already exists
+        found = None
+        def not_found(item):
+            nonlocal found
+            score = item['score']
+            del item['score']
+            if fnd := item == params:
+                print("Found in previous sweep")
+                found = score
+            item['score'] = score
+            return not fnd
+        previous = list(filter(not_found, previous))
+
         # Run cross-validation with the chosen hyperparameters
         try:
-            params['score'] = cross_validate(lambda fees: model(fees, **params), **cross_args)
-            print(f"Result: ${params['score']:.2f}")
+            params['score'] = found or cross_validate(lambda fees: model(fees, **params), **cross_args)
+            print(f"Result: {params['score']}")
         except KeyboardInterrupt:
             break
         except Exception as e:
@@ -93,3 +83,59 @@ def possible(config):
     while curr <= config['max']:
         yield curr
         curr = op(curr, config['by'])
+
+
+def gen_config(model: Type[Model], cross_args):
+    """Generate sweep configuration for logging."""
+    stock, text = cross_args['train_val']
+    assert(len(stock) == len(text))
+    for s, t in zip(stock, text):
+        assert(s.columns.name == t.columns.name)
+    if 'baseline_factory' not in cross_args:
+        cross_args['baseline_factory'] = lambda fees: Hold(fees)
+    return {
+        'model': model.__name__,
+        'simulation': {
+            'fees': cross_args['sim'].fees,
+            'principal': cross_args['sim'].principal,
+        },
+        'validation': {
+            'method': cross_args['partition'].__name__,
+            'split': cross_args['split'],
+            'folds': cross_args['folds'],
+        },
+        'data': {
+            'stock': [s.columns.name for s in stock],
+            'len_stock': [len(s) for s in stock],
+            'len_text': [len(t) for t in text],
+            'begin': str(min(s.iloc[0].date_time for s in stock)),
+            'end': str(max(s.iloc[-1].date_time for s in stock)),
+        },
+        'baseline': cross_args['baseline_factory'](cross_args['sim'].fees).__class__.__name__
+            if cross_args['baseline_factory'] is not None else 'None',
+        'sweep': list(),
+    }
+
+
+def check_previous(filename, results):
+    """Check for existing sweep and its validity."""
+    previous = []
+    if os.path.exists(filename):
+        print(f"Found existing sweep {filename}")
+        prev_results = None
+        with open(filename, 'r') as infile:
+            try:
+                prev_results = load(infile)
+            except JSONDecodeError:
+                print(f"ERROR: could not load sweep")
+        if prev_results:
+            previous = prev_results['sweep']
+            prev_results['sweep'] = list()
+            if prev_results != results:
+                print(f"WARNING: does not match current configuration")
+                previous = []
+        if not previous:
+            if not confirm(f"Delete {filename}?"):
+                print(f"Exiting...")
+                raise SystemExit
+    return previous
